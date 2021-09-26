224e0a38dd701f4614bab33f8637d2837a74ddce
JY Fang <junyuan.fang@helsinki.fi>
2021-09-26 20:22:27 +0300

    tables are now fine

--- schema.sql
+++ schema.sql
@@ -1,7 +1,26 @@
+CREATE TABLE forums (
+    forum_id SERIAL PRIMARY KEY,
+    theme TEXT NOT NULL,
+    visibility BOOLEAN NOT NULL
+);
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    forum_id INTEGER REFERENCES forums NOT NULL,
+    account VARCHAR(30) UNIQUE,
+    password VARCHAR(30)
+);
+
+CREATE TABLE user_forum (
+    forum_id INTEGER REFERENCES forums NOT NULL,
+    user_id INTEGER REFERENCES users NOT NULL,
+    isOwner INTEGER NOT NULL
+);
+
 CREATE TABLE messages (
     message_id SERIAL PRIMARY KEY,
     topic_id INTEGER REFERENCES topic NOT NULL,
-    user_id INTEGER REFERENCES user NOT NULL,
+    user_id INTEGER REFERENCES users NOT NULL,
     content TEXT NOT NULL,
     visibility BOOLEAN NOT NULL
 
@@ -9,36 +28,16 @@ CREATE TABLE messages (
 
 CREATE TABLE topic (
     topic_id SERIAL PRIMARY KEY,
-    forum_id INTEGER REFERENCES topic NOT NULL,
-    user_id INTEGER REFERENCES user NOT NULL,
+    forum_id INTEGER REFERENCES forums NOT NULL,
+    user_id INTEGER REFERENCES users NOT NULL,
     title TEXT NOT NULL,
     visibility BOOLEAN NOT NULL
 );
 
-CREATE TABLE forum (
-    forum_id SERIAL PRIMARY KEY,
-    theme TEXT NOT NULL,
-    visibility BOOLEAN NOT NULL
-    
-);
-
-CREATE TABLE user_forum (
-    forum_id INTEGER REFERENCES user NOT NULL,
-    user_id INTEGER REFERENCES user NOT NULL,
-    isOwner INTEGER NOT NULL
-);
-
-CREATE TABLE user (
-    user_id SERIAL PRIMARY KEY,
-    forum_id INTEGER REFERENCES user NOT NULL,
-    account VARCHAR(30) UNIQUE,
-    password VARCHAR(30),
-);
-
 CREATE TABLE request (
     request_id SERIAL PRIMARY KEY,
-    user_id_asking INTEGER REFERENCES user NOT NULL,
-    forum_id_to INTEGER REFERENCES forum NOT NULL,
-    user_id_admin INTEGER REFERENCES user NOT NULL,
+    user_id_asking INTEGER REFERENCES users NOT NULL,
+    forum_id_to INTEGER REFERENCES forums NOT NULL,
+    user_id_admin INTEGER REFERENCES users NOT NULL,
     confirmValue BOOLEAN NOT NULL
 );
