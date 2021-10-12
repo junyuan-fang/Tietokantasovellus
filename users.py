270b88a81e2c0350b6fd9a87e6c2cef434a20266
JY Fang <junyuan.fang@helsinki.fi>
2021-10-12 16:47:36 +0300

    query of forum is done

--- users.py
+++ users.py
@@ -44,13 +44,93 @@ def register(username, password):
     return login(username,password)
 
 def get_forums(user_id):
-    #SELECT-line need to be changed later, because time stamp is not included
-    # sql = "SELECT F.theme, F.public"\
-    #       "FROM user_forum UF"\
-    #       "INNER JOIN users U on U.user_id=UF.user_id"\
-    #       "INNER JOIN forums F on F.forum_id=UF.forum_id"\
-    #       "WHERE U.user_id=: user_id"
-    sql = "SELECT F.forum_id, F.theme, F.public, F.visibility FROM user_forum UF INNER JOIN users U on U.user_id=UF.user_id INNER JOIN forums F on F.forum_id=UF.forum_id WHERE U.user_id=(:user_id)"
+    # SELECT FT.theme, FT.public, FT.topic_num, FT.message_num, FT.time_stamp
+    # FROM user_forum UF
+    # INNER JOIN users U on U.user_id=UF.user_id
+    # INNER JOIN 
+
+    # (SELECT F.forum_id AS forum_id, F.theme AS theme, F.public, COUNT( TT.topic_id) AS topic_num, SUM(TT.message_num) AS message_num, MAX(TT.time_stamp) AS time_stamp
+    # FROM forums F
+    # INNER JOIN
+    # (SELECT T.topic_id AS topic_id, T.title, T.forum_id AS forum_id, COUNT(*) AS message_num, MAX(M.created_at) AS time_stamp
+    # FROM topic T 
+    # INNER JOIN messages M ON M.topic_id=T.topic_id 
+    # WHERE T.visibility=True AND M.visibility=True GROUP BY T.topic_id) AS TT
+    #  ON F.forum_id=TT.forum_id AND F.visibility=True
+    #  GROUP BY F.forum_id) AS FT
+
+    #  on FT.forum_id=UF.forum_id
+
+    #  WHERE U.user_id=: user_id or FT.public=True
+
+    #  ORDER BY FT.time_stamp DESC
+
+
+    # sql = "SELECT F.forum_id, F.theme, F.public, F.visibility FROM user_forum UF INNER JOIN users U on U.user_id=UF.user_id INNER JOIN forums F on F.forum_id=UF.forum_id WHERE U.user_id=(:user_id) OR F.public=True"
+    # inner_TT="(SELECT T.topic_id AS topic_id, T.title, T.forum_id AS forum_id, COUNT(*) AS message_num, MAX(M.created_at) AS time_stamp"/
+    # "FROM topic T "/
+    # "INNER JOIN messages M ON M.topic_id=T.topic_id "/
+    # "WHERE T.visibility=True AND M.visibility=True GROUP BY T.topic_id) AS TT"/
+
+    # inner_FT=" (SELECT F.forum_id AS forum_id, F.theme AS theme, F.public, COUNT( TT.topic_id) AS topic_num, SUM(TT.message_num) AS message_num, MAX(TT.time_stamp) AS time_stamp"/
+    # "FROM forums F"/
+    # "INNER JOIN" +inner_TT/
+    # "ON F.forum_id=TT.forum_id AND F.visibility=True"/
+    # "GROUP BY F.forum_id) AS FT"/
+
+    # sql= "SELECT FT.theme, FT.public, FT.topic_num, FT.message_num, FT.time_stamp"/
+    #  "FROM user_forum UF"/
+    #  "INNER JOIN users U on U.user_id=UF.user_id"/
+    #  "INNER JOIN"+inner_FT/
+    #   "on FT.forum_id=UF.forum_id"/
+    #  "WHERE U.user_id=: user_id or FT.public=True"/
+    #  "ORDER BY FT.time_stamp DESC"/
+
+    #sql="SELECT F.forum_id, F.theme, F.public, F.visibility FROM user_forum UF INNER JOIN users U on U.user_id=UF.user_id INNER JOIN forums F on F.forum_id=UF.forum_id WHERE U.user_id=(:user_id) OR F.public=True"
+    sql="""SELECT  FT.public public, FT.theme theme, FT.topic_num topic_num, FT.message_num message_num, FT.time_stamp time_stamp, FT.forum_id forum_id
+    FROM user_forum UF
+    INNER JOIN users U on U.user_id=UF.user_id
+    INNER JOIN 
+
+    (SELECT F.forum_id AS forum_id, F.theme AS theme, F.public, COUNT( TT.topic_id) AS topic_num, SUM(TT.message_num) AS message_num, MAX(TT.time_stamp) AS time_stamp
+    FROM forums F
+    INNER JOIN
+    (SELECT T.topic_id AS topic_id, T.title, T.forum_id AS forum_id, COUNT(*) AS message_num, MAX(M.created_at) AS time_stamp
+    FROM topic T 
+    INNER JOIN messages M ON M.topic_id=T.topic_id 
+    WHERE T.visibility=True AND M.visibility=True GROUP BY T.topic_id) AS TT
+     ON F.forum_id=TT.forum_id AND F.visibility=True
+     GROUP BY F.forum_id) AS FT
+
+     on FT.forum_id=UF.forum_id
+
+     WHERE U.user_id=:user_id or FT.public=True
+
+     ORDER BY FT.time_stamp DESC"""
     result = db.session.execute(sql, {"user_id": user_id})
     return result.fetchall()
-        
+
+
+
+
+
+    # """SELECT FT.theme, FT.public, FT.topic_num, FT.message_num, FT.time_stamp
+    # FROM user_forum UF
+    # INNER JOIN users U on U.user_id=UF.user_id
+    # INNER JOIN 
+
+    # (SELECT F.forum_id AS forum_id, F.theme AS theme, F.public, COUNT( TT.topic_id) AS topic_num, SUM(TT.message_num) AS message_num, MAX(TT.time_stamp) AS time_stamp
+    # FROM forums F
+    # INNER JOIN
+    # (SELECT T.topic_id AS topic_id, T.title, T.forum_id AS forum_id, COUNT(*) AS message_num, MAX(M.created_at) AS time_stamp
+    # FROM topic T 
+    # INNER JOIN messages M ON M.topic_id=T.topic_id 
+    # WHERE T.visibility=True AND M.visibility=True GROUP BY T.topic_id) AS TT
+    #  ON F.forum_id=TT.forum_id AND F.visibility=True
+    #  GROUP BY F.forum_id) AS FT
+
+    #  on FT.forum_id=UF.forum_id
+
+    #  WHERE U.user_id=3 or FT.public=True
+
+    #  ORDER BY FT.time_stamp DESC"""
